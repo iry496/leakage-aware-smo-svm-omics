b9d36c14e6d669c215ff92655780309a9e8bcac8
Iris Yang <iry496@g.harvard.edu>
2026-07-03 01:18:26 -0700

    Delete scripts/02_run_leaky_baseline.R

--- scripts/02_run_leaky_baseline.R
+++
@@ -1,9 +0,0 @@
-# Run intentionally leaky baseline for pilot.
-# WARNING: This script intentionally includes global feature selection for comparison only.
-
-source("R/pipelines.R")
-
-# TODO:
-# Load discovery expression matrix and labels.
-# Run global feature selection + CV.
-# Export results/pilot/leaky_baseline_metrics.csv.
