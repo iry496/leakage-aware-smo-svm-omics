22aa7085ab9b843a85ec6c9cd69f47f16aeaaf3f
Iris Yang <iry496@g.harvard.edu>
2026-07-03 01:54:59 -0700

    Delete scripts/04_feature_stability.R

--- scripts/04_feature_stability.R
+++
@@ -1,8 +0,0 @@
-# Feature-stability analysis.
-
-source("R/stability.R")
-
-# TODO:
-# Load selected feature sets from guarded nested pipeline.
-# Compute Nogueira stability, Jaccard overlap, and selection frequencies.
-# Export tables/feature_stability_results.csv.
