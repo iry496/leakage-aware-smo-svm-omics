e920438d9ef534d9998fd30653bf437899803c0f
Iris Yang <iry496@g.harvard.edu>
2026-07-03 02:45:56 -0700

    Delete scripts/06_make_figures_tables.R

--- scripts/06_make_figures_tables.R
+++
@@ -1,9 +0,0 @@
-# Manuscript figures and tables.
-
-# TODO:
-# Figure 1: naive vs guarded workflow
-# Figure 2: dataset independence map
-# Figure 3: leakage inflation
-# Figure 4: feature stability
-# Figure 5: external validation
-# Figure 6: evidence audit dashboard
